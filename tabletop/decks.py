eb1818448bbc2c0fc64aec62bd8fedcc2e9daeea
Cameron Seebach <cseebach@sinfoniasolutions.com>
2015-07-18 15:25:47 +0300

    add deck definitions for factions other than gearpunk

--- tabletop/decks.py
+++ tabletop/decks.py
@@ -2,5 +2,17 @@ decks = {
     "gearpunk": [
         ["mine", 20],
         ["uranium_purifier", 8],
+    ],
+    "dryad": [
+        ["mine", 20],
+        ["uranium_purifier", 8],
+    ],
+    "ice": [
+        ["mine", 20],
+        ["uranium_purifier", 8],
+    ],
+    "minotaur": [
+        ["mine", 20],
+        ["uranium_purifier", 8],
     ]
 }
